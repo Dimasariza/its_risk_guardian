fa629a118705c1562f2a20c02746153d11f3dfb6
Dimasariza <roganda.sihombing11@gmail.com>
2024-04-23 15:24:16 +0700

    change next config to standard

--- next.config.js
+++ next.config.js
@@ -1,30 +1,30 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//     output: "export",
-//     // reactStrictMode: true,
-//     basePath: "/rbits",
-//     assetPrefix: '/rbits/',
-// }
+const nextConfig = {
+    output: "export",
+    reactStrictMode: true,
+    basePath: "/rbits",
+    assetPrefix: '/rbits/',
+}
 
-// module.exports = nextConfig
+module.exports = nextConfig
 
-const withPlugins = require('next-compose-plugins');
-const optimizedImages = require('next-optimized-images');
-module.exports = withPlugins([
-    [optimizedImages, {
-        mozjpeg: {
-            quality: 80,    
-        },
-            pngquant: {
-            speed: 3,
-            strip: true,
-            verbose: true,
-        },
-        imagesPublicPath: '/rbits/_next/static/images/',
-    }],
-    {
-        basePath: '/rbits',
-        assetPrefix: '/rbits/',
-        // env,
-    },
-]);
+// const withPlugins = require('next-compose-plugins');
+// const optimizedImages = require('next-optimized-images');
+// module.exports = withPlugins([
+//     [optimizedImages, {
+//         mozjpeg: {
+//             quality: 80,    
+//         },
+//             pngquant: {
+//             speed: 3,
+//             strip: true,
+//             verbose: true,
+//         },
+//         imagesPublicPath: '/rbits/_next/static/images/',
+//     }],
+//     {
+//         basePath: '/rbits',
+//         assetPrefix: '/rbits/',
+//         // env,
+//     },
+// ]);
