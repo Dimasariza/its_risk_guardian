3d73da10b9907047cd1a70a12715fe72ca66d7e4
Dimasariza <roganda.sihombing11@gmail.com>
2024-04-23 16:23:16 +0700

    try

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 'use client';
 import { LayoutProvider } from '../layout/context/layoutcontext';
 import { PrimeReactProvider } from 'primereact/api';
-import 'primereact/resources/primereact.css';
-import 'primeflex/primeflex.css';
-import 'primeicons/primeicons.css';
+import '.primereact/resources/primereact.css';
+import '.primeflex/primeflex.css';
+import '.primeicons/primeicons.css';
 import '../styles/layout/layout.scss';
 import '../styles/demo/Demos.scss';
 
