0e2734f55dec83f7fce0040f8798225230aba204
Dimasariza <roganda.sihombing11@gmail.com>
2024-05-26 19:44:33 +0700

    add photo profile

--- app/(main)/assets/asset-register/tab-menu/documents.tsx
+++ app/(main)/assets/asset-register/tab-menu/documents.tsx
@@ -82,15 +82,15 @@ function Documents() {
     const emptyTemplate = () => {
       return (
         <div className="flex align-items-center flex-column">
-          <i className="pi pi-image mt-3 p-5" style={{ fontSize: '5em', borderRadius: '50%', backgroundColor: 'var(--surface-b)', color: 'var(--surface-d)' }}></i>
+          <i className="pi pi-file-pdf mt-3 p-5" style={{ fontSize: '5em', borderRadius: '50%', backgroundColor: 'var(--surface-b)', color: 'var(--surface-d)' }}></i>
           <span style={{ fontSize: '1.2em', color: 'var(--text-color-secondary)' }} className="my-5">
-              Drag and Drop Image Here
+              Drag and Drop PDF Here
           </span>
         </div>
       );
     };
 
-    const chooseOptions = { icon: 'pi pi-fw pi-images', iconOnly: true, className: 'custom-choose-btn p-button-rounded p-button-outlined' };
+    const chooseOptions = { icon: 'pi pi-fw pi-file-pdf', iconOnly: true, className: 'custom-choose-btn p-button-rounded p-button-outlined' };
     const uploadOptions = { icon: 'pi pi-fw pi-cloud-upload', iconOnly: true, className: 'custom-upload-btn p-button-success p-button-rounded p-button-outlined' };
     const cancelOptions = { icon: 'pi pi-fw pi-times', iconOnly: true, className: 'custom-cancel-btn p-button-danger p-button-rounded p-button-outlined' };
 
@@ -114,7 +114,7 @@ function Documents() {
       <Tooltip target=".custom-upload-btn" content="Upload" position="bottom" />
       <Tooltip target=".custom-cancel-btn" content="Clear" position="bottom" />
 
-      <FileUpload ref={fileUploadRef} name="demo[]" url="/api/upload" multiple accept="image/*" maxFileSize={1000000}
+      <FileUpload ref={fileUploadRef} name="demo[]" url="/api/upload" multiple accept="application/pdf" maxFileSize={1000000}
         onUpload={onTemplateUpload} onSelect={onTemplateSelect} onError={onTemplateClear} onClear={onTemplateClear}
         headerTemplate={headerTemplate} itemTemplate={itemTemplate} emptyTemplate={emptyTemplate}
         chooseOptions={chooseOptions} uploadOptions={uploadOptions} cancelOptions={cancelOptions} />
