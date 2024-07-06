c232bfb308082930fc83a6cd89b5b0ea34eb2512
dimasariza <dimas.ariza20@gmail.com>
2024-07-07 06:22:37 +0700

    add cumulative distribution normal function

--- function/calculateThinningValue.ts
+++ function/calculateThinningValue.ts
@@ -96,7 +96,12 @@ export const parameterBCoef = {
     damageState3: 4,
 }
 
-export const NORMSDIST = (z: number) => {
-    return 1 / ((2 * Math.PI) * - Math.exp(z ** 2 / 2))
-}
+function ncdf(x: number, mean: number = 0, std: number = 1) {
+    var x = (x - mean) / std
+    var t = 1 / (1 + .2315419 * Math.abs(x))
+    var d =.3989423 * Math.exp( -x * x / 2)
+    var prob = d * t * (.3193815 + t * ( -.3565638 + t * (1.781478 + t * (-1.821256 + t * 1.330274))))
+    if( x > 0 ) prob = 1 - prob
+    return prob
+  }
 
