361cdb364e56fb79c88534bfcfc77cfb93879ff5
dimasariza <dimas.ariza20@gmail.com>
2024-07-06 23:20:15 +0700

    update damage mechanism

--- app/(main)/calculation/risk-based-inspection/tab-menu/damage-mechanism/damage-mechanism.tsx
+++ app/(main)/calculation/risk-based-inspection/tab-menu/damage-mechanism/damage-mechanism.tsx
@@ -12,7 +12,7 @@ import { damageMechanismService } from '@/service/calculation/damageMechanism-se
 import { Toast } from 'primereact/toast';
 
 function DamageMechanism() {
-  const uncheckedAll = {
+  const initialChild = {
     dm_thinning: false,
     dm_comp_linning: false,
     dm_scc_caustic: false,
@@ -21,13 +21,11 @@ function DamageMechanism() {
     dm_scc_hic_h2s: false,
     dm_scc_alkaline: false,
     dm_scc_polythionic: false,
-    dm_scc_chloride: false, // parent
     dm_scc_chloride_A: false,
     dm_scc_chloride_B: false,
     dm_scc_chloride_C: false,
     dm_scc_hydrogen: false,
     dm_scc_hic_hf: false,
-    dm_exCor: false, // parent
     dm_exCor_A: false,
     dm_exCor_B: false,
     dm_exCor_C: false,
@@ -38,118 +36,89 @@ function DamageMechanism() {
     dm_exCor_H: false,
     dm_exCor_I: false,
     dm_cor_under_ins: false,
-    dm_exChloride: false, // parent
     dm_exChloride_A: false,
     dm_exChloride_B: false,
     dm_exChloride_C: false,
     dm_exChloride_D: false,
-    dm_highTemp: false, // parent
     dm_highTemp_A: false,
     dm_highTemp_B: false,
     dm_highTemp_C: false,
-    dm_brittleFracture: false, // parent
     dm_brittleFracture_A: false,
     dm_brittleFracture_B: false,
-    dm_low_alloy_steel: false, // parent
     dm_low_alloy_steel_A: false,
     dm_low_alloy_steel_B: false,
-    dm_embrittlement: false, // parent
     dm_embrittlement_A: false,
     dm_embrittlement_B: false,
-    dm_sigma_phase: false, // parent
     dm_sigma_phase_A: false,
     dm_sigma_phase_B: false,
-    dm_piping_mechanical: false, // parent
     dm_piping_mechanical_A: false,
     dm_piping_mechanical_B: false
   };
 
+  const initialParent = {
+    dm_scc_chloride: false, // parent
+    dm_exCor: false, // parent
+    dm_exChloride: false, // parent
+    dm_highTemp: false, // parent
+    dm_brittleFracture: false, // parent
+    dm_low_alloy_steel: false, // parent
+    dm_embrittlement: false, // parent
+    dm_sigma_phase: false, // parent
+    dm_piping_mechanical: false, // parent
+  }
+
   const toast = useRef<any>(null);
-  const [checked, setChecked] = useState<any>(uncheckedAll);
+  const [checked, setChecked] = useState<any>(initialChild);
+  const [parentChecked, setParentChecked] = useState<any>(initialParent);
   const [error, setError] = useState<any | null>({});
   const [disabled, setDisabled] = useState(true);
   const data = useSelector((state: any) => state.Reducer);
   const dispatchEdit = useDispatch();
   let edit = useSelector((state: any) => state.EditReducer);
 
-  const handleChecked = (e: any, checkedValue: any) => {
-    setChecked((prev: any) => ({ ...prev, [checkedValue]: e.checked }))
-  }
-
   useEffect(() => {
-    setChecked((prev: any) => ({ 
+    setParentChecked((prev: any) => ({ 
       ...prev, 
       dm_scc_chloride:  
-      checked.dm_scc_chloride_A &&
-      checked.dm_scc_chloride_B &&
-      checked.dm_scc_chloride_C,
+        checked.dm_scc_chloride_A &&
+        checked.dm_scc_chloride_B &&
+        checked.dm_scc_chloride_C,
       dm_exCor:
-      checked.dm_exCor_A &&
-      checked.dm_exCor_B &&
-      checked.dm_exCor_C &&
-      checked.dm_exCor_D &&
-      checked.dm_exCor_E &&
-      checked.dm_exCor_F &&
-      checked.dm_exCor_G &&
-      checked.dm_exCor_H &&
-      checked.dm_exCor_I,
+        checked.dm_exCor_A &&
+        checked.dm_exCor_B &&
+        checked.dm_exCor_C &&
+        checked.dm_exCor_D &&
+        checked.dm_exCor_E &&
+        checked.dm_exCor_F &&
+        checked.dm_exCor_G &&
+        checked.dm_exCor_H &&
+        checked.dm_exCor_I,
       dm_exChloride:
-      checked.dm_exChloride_A &&
-      checked.dm_exChloride_B &&
-      checked.dm_exChloride_C &&
-      checked.dm_exChloride_D,
+        checked.dm_exChloride_A &&
+        checked.dm_exChloride_B &&
+        checked.dm_exChloride_C &&
+        checked.dm_exChloride_D,
       dm_highTemp:
-      checked.dm_highTemp_A &&
-      checked.dm_highTemp_B &&
-      checked.dm_highTemp_C,
+        checked.dm_highTemp_A &&
+        checked.dm_highTemp_B &&
+        checked.dm_highTemp_C,
       dm_brittleFracture:
-      checked.dm_brittleFracture_A &&
-      checked.dm_brittleFracture_B,
+        checked.dm_brittleFracture_A &&
+        checked.dm_brittleFracture_B,
       dm_low_alloy_steel:
-      checked.dm_low_alloy_steel_A &&
-      checked.dm_low_alloy_steel_B,
+        checked.dm_low_alloy_steel_A &&
+        checked.dm_low_alloy_steel_B,
       dm_embrittlement:
-      checked.dm_embrittlement_A &&
-      checked.dm_embrittlement_B,
+        checked.dm_embrittlement_A &&
+        checked.dm_embrittlement_B,
       dm_sigma_phase:
-      checked.dm_sigma_phase_A &&
-      checked.dm_sigma_phase_B,
+        checked.dm_sigma_phase_A &&
+        checked.dm_sigma_phase_B,
       dm_piping_mechanical: 
-      checked.dm_piping_mechanical_A && 
-      checked.dm_piping_mechanical_B,
+        checked.dm_piping_mechanical_A && 
+        checked.dm_piping_mechanical_B,
     }))
-  }, [
-    checked.dm_scc_chloride_A,
-    checked.dm_scc_chloride_B,
-    checked.dm_scc_chloride_C,
-    checked.dm_exCor_A,
-    checked.dm_exCor_B,
-    checked.dm_exCor_C,
-    checked.dm_exCor_D,
-    checked.dm_exCor_E,
-    checked.dm_exCor_F,
-    checked.dm_exCor_G,
-    checked.dm_exCor_H,
-    checked.dm_exCor_I,
-    checked.dm_exChloride_A,
-    checked.dm_exChloride_B,
-    checked.dm_exChloride_C,
-    checked.dm_exChloride_D,
-    checked.dm_highTemp_A,
-    checked.dm_highTemp_B,
-    checked.dm_highTemp_C,
-    checked.dm_brittleFracture_A,
-    checked.dm_brittleFracture_B,
-    checked.dm_low_alloy_steel_A,
-    checked.dm_low_alloy_steel_B,
-    checked.dm_embrittlement_A,
-    checked.dm_embrittlement_B,
-    checked.dm_sigma_phase_A,
-    checked.dm_sigma_phase_B,  
-    checked.dm_piping_mechanical_A, 
-    checked.dm_piping_mechanical_B,
-  ])
-
+  }, [checked])
 
   useEffect(() => {
     edit = true; // to disabled edit useeffect in first call
@@ -161,7 +130,7 @@ function DamageMechanism() {
       });
     } else {
       setDisabled(true);
-      setChecked(uncheckedAll);
+      setChecked(initialChild);
     }
   }, [data]);
 
@@ -200,8 +169,16 @@ function DamageMechanism() {
         <Checkbox 
           name={checkedValue}
           readOnly={typeof screeningCriteria == 'object'} 
-          onChange={(e) => handleChecked(e, checkedValue)} 
-          checked={checked[checkedValue]} 
+          onChange={(e) => {
+            typeof screeningCriteria == 'object'
+            ? setParentChecked((prev: any) => ({ ...prev, [checkedValue]: e.checked }))
+            : setChecked((prev: any) => ({ ...prev, [checkedValue]: e.checked }))
+          }} 
+          checked={
+            typeof screeningCriteria == 'object' 
+            ? parentChecked[checkedValue] 
+            : checked[checkedValue]
+          } 
           disabled={disabled || !edit}
         >
         </Checkbox>
@@ -221,7 +198,7 @@ function DamageMechanism() {
                   <Checkbox 
                     name={checkedValue}
                     className="col-2 justify-content-end flex  p-0 align-self-center" 
-                    onChange={(e) => handleChecked(e, checkedValue) } 
+                    onChange={(e) => setChecked((prev: any) => ({ ...prev, [checkedValue]: e.checked }))} 
                     checked={checked[checkedValue]} 
                     disabled={disabled || !edit}
                     >
