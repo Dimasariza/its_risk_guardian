d6de53a8a036e663c6e965d310adf9a9d50b4de3
dimasariza <dimas.ariza20@gmail.com>
2024-07-18 08:19:22 +0700

    add logo its

--- layout/AppTopbar.tsx
+++ layout/AppTopbar.tsx
@@ -57,6 +57,11 @@ const AppTopbar = forwardRef<AppTopbarRef>((props, ref) => {
         <i className="pi pi-bars" />
       </button>
 
+      <div className='flex w-full justify-content-center'>
+        {/* <Avatar image={`${process.env.DB_URL}/file/${data.user?.user_picture}`} size="large" style={{ backgroundColor: '#9c27b0', color: '#ffffff' }} shape="circle" className="ml-2" /> */}
+        <img src={`${process.env.PUBLIC_URL}/layout/images/ITS.png`} width="50px" height={'50px'} alt="logo" />
+      </div>
+
       <button ref={topbarmenubuttonRef} type="button" className="p-link layout-topbar-menu-button layout-topbar-button" onClick={showProfileSidebar}>
         <i className="pi pi-ellipsis-v" />
       </button>
